85412e0e9db948171ae765244290458bec945eb2
matteocicigoi <matteo.cicigoi1@hotmail.it>
2024-01-26 14:15:23 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- css + js -->
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <h1>Laravel Template</h1>
+    <!-- esempio immagine
+    <img src="{{ Vite::asset('resources/img/logo.png') }}" alt="">
+    -->
+</body>
+</html>
